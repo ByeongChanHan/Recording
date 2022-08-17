5e18c67cc6a67f6891e46238a4be6132a9b89182
HanByungChan <gks5927@gmail.com>
2022-08-17 13:42:41 +0900

    테스트용

---
+++ app.js
@@ -0,0 +1,55 @@
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.interimResults = true; //아직 인식이 완료되지 않은 중간 결과 값
+recognition.lang = "ko-KR"; //언어
+
+let isRecord = false; //녹음 여부 변수
+
+//<p class = "para"> 태그 생성
+let p = document.createElement("p");
+p.classList.add("para");
+
+// words 자식으로 문구 추가
+let words = document.querySelector(".words");
+words.appendChild(p);
+
+let mic = document.querySelector("#circlein"); //마이크 버튼
+let speechToText = "";
+
+recognition.addEventListener("result", (e) => {
+  let interimTranscript = "";
+  let len = e.results.length;
+  for (let i = e.resultIndex; i < len; i++) {
+    let transcript = e.results[i][0].transcript; //인식된 단어(String) 값을 저장
+    //만약 끝이 나면 이어 붙이기
+    if (e.results[i].isFinal) {
+      speechToText += transcript;
+    } else {
+      interimTranscript += transcript;
+    }
+  }
+  //녹음을 마쳤을때 호출
+  recognition.addEventListener("soundend", () => {
+    mic.style.backgroundColor = null;
+  });
+
+  document.querySelector(".para").innerHTML = speechToText + interimTranscript;
+});
+
+//마이크 클릭시 호출
+mic.addEventListener("click", () => {
+  if (isRecord == false) {
+    console.log("마이크켜짐");
+    recognition.start();
+    isRecord = true;
+    mic.style.backgroundColor = "#6BD6E1";
+  } else {
+    console.log("마이크꺼짐");
+    recognition.stop();
+    isRecord = false;
+    mic.style.backgroundColor = null;
+  }
+});
